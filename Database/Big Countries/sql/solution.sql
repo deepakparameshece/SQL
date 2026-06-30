b16eee891c45b357b09ee6d8685e57e3b77f3ea4
Deepak P <deepak.vsbec@gmail.com>
2026-06-30 14:45:18 +0530

    feat: Add Big Countries

--- Database/Big Countries/sql/solution.sql
+++ Database/Big Countries/sql/solution.sql
@@ -1,3 +1,3 @@
 # Write your MySQL query statement below
-Select name , population, area from World
-Where area>=3000000 || population >25000000;
+select name,population,area from World
+WHere population>=25000000 or area>=3000000;
